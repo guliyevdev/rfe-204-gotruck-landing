10280302e4b87c1c4878182fb5878c922b9a3651
guliyevdev <q.behruz1@gmail.com>
2024-09-02 11:49:40 +0400

    feat: Add blog section to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { FeaturesSection } from "@/app/components/sections/main/features";
 import { ClientSection } from "@/app/components/sections/main/client";
 import { TestimonialSection } from "@/app/components/sections/main/testimonial";
 import { FaqSection } from "@/app/components/sections/main/faq";
+import { BlogSection } from "./components/sections/main/blogs";
 
 export default function Home() {
   return (
@@ -19,6 +20,9 @@ export default function Home() {
       {/* =========== testimonial Section Start =========== */}
       {/* <TestimonialSection /> */}
       {/* =========== testimonial Section end =========== */}
+      {/* =========== blogs Section start =========== */}
+      <BlogSection />
+      {/* =========== blogs Section end =========== */}
       {/* =========== faq Section start =========== */}
        <FaqSection /> 
       {/* =========== faq Section end =========== */}
